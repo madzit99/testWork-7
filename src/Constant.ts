5011f5ea2242fc1cb165f90c328bd36d763af03d
Aliev Madjit <madzitaliev51@gmail.com>
2024-06-01 14:40:38 +0600

    Создал файл tupe.d.ts (типы) и константы

---
+++ src/Constant.ts
@@ -0,0 +1,21 @@
+import burgerImage from "../assets/burger.jpeg";
+import lemonadeImage from "../assets/lemonade.png";
+import donerImage from "../assets/doner.jpeg";
+import teaImage from "../assets/tea.jpeg";
+import sendfichImage  from "../assets/sendfich.png";
+import friesImage from "../assets/fries.jpeg";
+import borjomiImage from "../assets/borjomi.jpeg";
+import coffeeImage from "../assets/coffee.jpeg";
+
+const FOOD = [
+  { name: "Burger", cost: 180, img: burgerImage },
+  { name: "Lemonade", cost: 120, img: lemonadeImage },
+  { name: "Doner", cost: 150, img: donerImage },
+  { name: "Tea", cost: 70, img: teaImage },
+  { name: "Sendfich", cost: 90, img: sendfichImage },
+  { name: "Fries", cost: 60, img: friesImage },
+  { name: "Borjomi", cost: 60, img: borjomiImage },
+  { name: "Coffee", cost: 30, img: coffeeImage },
+];
+
+export default FOOD;
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+export interface FoodProps {
+  name: string;
+  cost: number;
+  img: string;
+};
+
+
